7f9ef0d643263e3b8933bbcf9b3a7f8da39d4c34
AnikinSimon <s.anikin1309@gmail.com>
2023-01-11 20:32:45 +0300

    Add cards

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,79 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+    <v-app-bar color="blue darken" dark app>
+      <v-app-bar-nav-icon @click="navigation = !navigation"></v-app-bar-nav-icon>
+      <v-toolbar-title>НОВОСТИ!! Лучшие новости!!!</v-toolbar-title>
     </v-app-bar>
-
+    <v-navigation-drawer v-model="navigation" absolute temporary>
+    </v-navigation-drawer>
     <v-main>
-      <HelloWorld/>
+      <template>
+        <v-container class="grey lighten-5">
+          <v-row no-gutters>
+            <v-col v-for="arcticle, index in news" :key="index" cols="12" sm="4">
+              <v-card>
+                <v-img :src="arcticle.urlToImage" class="white--text align-end"
+                  gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)" height="200px">
+                  <v-card-title>{{ arcticle.title }}</v-card-title>
+                </v-img>
+                <v-card-text>
+                  <p>
+                    {{ arcticle.content }}
+                  </p>
+                </v-card-text>
+                <v-card-actions>
+
+                  <v-btn variant="outlined" :href="arcticle.url">
+                     Читать полностью 
+                  </v-btn>
+                  <v-spacer></v-spacer>
+                  <v-btn icon>
+                    <v-icon>mdi-heart</v-icon>
+                  </v-btn>
+
+                  <v-btn icon>
+                    <v-icon>mdi-bookmark</v-icon>
+                  </v-btn>
+
+                  <v-btn icon>
+                    <v-icon>mdi-share-variant</v-icon>
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </template>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
   },
 
   data: () => ({
-    //
+    navigation: false,
+    news: []
   }),
+  methods: {
+    getNews() {
+      this.axios({
+        method: "GET",
+        url: "https://newsapi.org/v2/top-headlines?country=us&apiKey=d7f41a32c26b4bbfb596d58b1a54c766"
+      }).then((response)=> {
+        this.news = response.data.articles
+        console.log(response.data.articles)
+      })
+    }
+  },
+  mounted() {
+    this.getNews()
+  }
 };
 </script>
